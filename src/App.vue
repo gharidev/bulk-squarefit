c53b63a5c9dcb83d38af682f3912c3289c64b4d6
G Haridev <haridevgireesh@gmail.com>
2022-08-24 02:49:10 +0530

    Redirect footer links to new tab

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     </v-main>
     <v-footer padless class="justify-center">
           <p class="text-center pa-4 ma-0">Crafted with ❤ by Haridev<br />
-            Links: <a href="https://github.com/gharidev">Github</a> | <a href="https://haridev.me">Portfolio</a></p>
+            Links: <a href="https://github.com/gharidev" target="_blank">Github</a> | <a href="https://haridev.me" target="_blank">Portfolio</a></p>
     </v-footer>
   </v-app>
 </template>
