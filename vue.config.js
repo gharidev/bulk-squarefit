1a36a34890c0a54e67538287fd3b5ca5c70b9c18
G Haridev <haridevgireesh@gmail.com>
2022-08-24 20:54:34 +0530

    Added PWA support

--- vue.config.js
+++ vue.config.js
@@ -13,4 +13,15 @@ module.exports = defineConfig({
   },
   outputDir: path.resolve(__dirname, "./docs/"),
   publicPath: '/bulk-squarefit/',
+  pwa: {
+    name: "Bulk SquareFit",
+    themeColor: "#8a00e6",
+    icons: {
+      favicon32: './public/favicon-32x32.png',
+      favicon16: './public/favicon-16x16.png',
+      appleTouchIcon: './public/apple-touch-icon.png',
+      maskIcon: './public/safari-pinned-tab.svg',
+      msTileImage: './public/mstile-150x150.png'
+    }
+  }
 })
