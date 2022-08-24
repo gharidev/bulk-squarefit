699fb645d1883d713cbcd9cce1af5e0f2ab2a9fa
G Haridev <haridevgireesh@gmail.com>
2022-08-24 21:49:18 +0530

    PWA updates

--- vue.config.js
+++ vue.config.js
@@ -16,12 +16,17 @@ module.exports = defineConfig({
   pwa: {
     name: "Bulk SquareFit",
     themeColor: "#8a00e6",
-    icons: {
+    msTileColor: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+    // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    iconPaths: {
       favicon32: './public/favicon-32x32.png',
       favicon16: './public/favicon-16x16.png',
       appleTouchIcon: './public/apple-touch-icon.png',
       maskIcon: './public/safari-pinned-tab.svg',
       msTileImage: './public/mstile-150x150.png'
-    }
+    },
   }
 })
